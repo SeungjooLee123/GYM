f98b68a1e8656c16e59a4651316b110a05c9a7ef
SeungjooLee123 <dltmdwn0630@gmail.com>
2021-04-13 21:26:03 +0900

    뭐야ㅑ 뭐야ㅑ

--- app/src/test/java/com/example/gym/ExampleUnitTest.kt
+++ app/src/test/java/com/example/gym/ExampleUnitTest.kt
@@ -8,6 +8,7 @@ import org.junit.Assert.*
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ * 뭐야 뭐야 뭐야ㅑㅑㅑ
  */
 class ExampleUnitTest {
     @Test
